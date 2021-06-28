72d645fa71cdca5042254e65b5678e9579bcf889
Kazooru <47382098+dylanbrudey@users.noreply.github.com>
2021-06-28 23:36:20 +0200

    add authentification

--- api/api.py
+++ api/api.py
@@ -1,7 +1,84 @@
 import time
-from flask import Flask
+from flask import Flask, g
+from flask.helpers import url_for
+from flask import request
+from flask_sqlalchemy import SQLAlchemy
+from passlib.apps import custom_app_context as pwd_context
+from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tmp/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+auth = HTTPBasicAuth()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+
+@app.route('/api/users', methods=['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400)  # missing arguments
+    if User.query.filter_by(username=username).first() is not None:
+        abort(400)  # existing user
+    user = User(username=username)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return {'username': user.username}, 201
+
+
+@auth.verify_password
+def verify_password(username, password):
+    username = request.json.get('username')
+    password = request.json.get('password')
+    user = User.query.filter_by(username=username).first()
+    if not user or not user.verify_password(password):
+        print('HOHOHO', flush=True)
+        print(username, flush=True)
+        print(password, flush=True)
+        print(user, flush=True)
+        return False
+    g.user = user
+    print(g.user, flush=True)
+    return True
+
+
+@auth.error_handler
+def unauthorized():
+    return {'error': 'error'}, 401
+
+
+@app.route('/api/resource', methods=['POST'])
+def verify_password():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    user = User.query.filter_by(username=username).first()
+    if not user or not user.verify_password(password):
+        print('HOHOHO', flush=True)
+        print(username, flush=True)
+        print(password, flush=True)
+        print(user, flush=True)
+        return {'response': 'false'}
+    g.user = user
+    print(g.user, flush=True)
+    return {'response': 'true'}
+
 
 @app.route('/time')
 def get_current_time():
--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
+import React, { useState, useEffect, useRef } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+  const user = useRef();
+  const password = useRef();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
+  const addUser = () => {
+    fetch('/api/users', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({'username': user.current.value, 'password': password.current.value})
+    }).then(res => res.json()).then(data => {
+      console.log(data)
+    });
+  }
+
+  const checkUser = () => {
+    fetch('/api/resource', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({'username': user.current.value, 'password': password.current.value})
+    }).then(res => res.json()).then(data => {
+      console.log(data)
+    });
+  }
+
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+        Login
+
+        {/* <p>The current time is {currentTime}.</p> */} 
+        <div className='flex justify-content-left col-3 card m-5 border-0'>
+          <label className=' text-dark' for='user'>User</label>
+          <input id='user' ref={user} className='form-control' type='text' />
+          <label className=' text-dark' for='user'>Password</label>
+          <input ref={password} className='form-control' type='password' />
+          <button type='button' className='btn btn-primary' onClick={addUser}>Add User</button>
+          <button type='button' className='btn btn-danger' onClick={checkUser}>Verify User</button>
+        </div>
       </header>
     </div>
   );
